286d91dee0d62c32dff41923fe7e850e4255f858
joshSteadman <josh.steadman15@gmail.com>
2020-09-16 16:36:49 +0100

    url error

--- index.js
+++ index.js
@@ -6,4 +6,31 @@ document.addEventListener('DOMContentLoaded', () => {
   noteList.createNote('NOte TWO !');
 
   notesController.getHtml();
+
+  function changeUrl() {
+    window.addEventListener("hashchange", 'note[1]')
+  }
+
+  function changeNoteOnClick(){
+    document.getElementById('note-1').addEventListener('click', function clickEvent() {
+     
+      console.log('hello')
+    })
+
+  }
+  
+changeNoteOnClick()
+  // function showNoteForPage() {
+  //   showNote(getNoteFromUrl(window.location))
+  // }
+
+  // function getNoteFromUrl(location){
+  //   return location.hash.split("#")[1];
+  // }
+
+  // function showNote(note) {
+  //   document.getElementById("app").innerHTML = note.id
+  // }
 });
+
+
---
+++ spec/single-note-controller-test.js
@@ -0,0 +1,11 @@
+// describe(SingleNoteController, () => {
+//     it('create a note and display it on the page', () => {
+//         let noteList = new NoteList();
+//         let notesController = new NoteController(noteList); 
+//         noteList.createNote('number one note')
+
+       
+//     })
+          
+         
+// })
